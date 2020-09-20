80fcf6653d58e6f721f20c50a6a450209ea234da
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2020-09-20 10:32:25 +0530

    jinjaa

--- run.py
+++ run.py
@@ -10,14 +10,33 @@ CORS(app)
 def paige():
    return render_template('index.html')
 
+@app.route('/quiz')
+def qnss():
+   return render_template('qns.html', all_qns = aq , all_ans = ica)
+
 @app.route('/quiz/send',methods=['POST', 'GET'])
 def main():
    if request.method == 'POST':
       resp_json = request.get_json()
       options = resp_json['deets']
-      print(options)
       qns = q.get_quiz(options[0],options[1])
-      print(json.dumps(qns,indent=4))
+      # print(json.dumps(qns,indent=4))
+      global aq
+      aq = []
+      global ica
+      ica = []
+      check = {}
+      for i in range(5):
+         check.update({qns["results"][i]["question"]:qns["results"][i]["correct_answer"]})     
+      for i in range(5):
+         aq.append(qns["results"][i]["question"])
+         interim = list(qns["results"][i]["incorrect_answers"])
+         interim.append(qns["results"][i]["correct_answer"])
+         ica.append(interim)
+      print(ica)
+      
+   # return render_template('qns.html', all_qns = aq , all_ans = ica)
+
    return json.dumps({"response": 'could be anything noone cares'}), 200
 
 
